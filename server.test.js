8db7c5d9de7dc4a6ebd1b83a527df3f6f1631819
hverma007 <harshitvermaa@hotmail.com>
2025-07-22 15:40:10 -0400

    changedservertest.js

--- server.test.js
+++ server.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
   it('Hello, World! , welcome to the world of Node.js', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toBe('Hello, World!');
+    expect(response.text).toBe('Hello, World! , welcome to the world of Node.js');
 });
 });
