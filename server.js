c5f579167f4c2c8cd78d507b265ac7af47b0f1bc
hverma007 <harshitvermaa@hotmail.com>
2025-07-22 14:47:16 -0400

    changed server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, World! , welcome to the world of Node.js');
 });
 
 // New route with a linting issue: unused variable
