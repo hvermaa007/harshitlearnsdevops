7721e3956f64a0678e8b95765642a0b808da363f
hverma007 <harshitvermaa@hotmail.com>
2025-07-22 15:37:49 -0400

    changed server.test.js

--- server.test.js
+++ server.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('./server');
 
 describe('GET /', () => {
-  it('should return Hello, World!', async () => {
+  it('Hello, World! , welcome to the world of Node.js', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
     expect(response.text).toBe('Hello, World!');
