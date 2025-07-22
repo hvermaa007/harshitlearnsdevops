644206d3d5586165b1cdb84519a445768bfd9457
hvermaa007 <harshitvermaa@hotmail.com>
2025-07-22 16:31:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,5 @@
 const app = require('./server');
 app.listen(3000, () => {
   console.log('Server running...');
-  const passwrd="rwewe@3432"
-  const DB_PASSWORD = 'P@ss123!';
-  const password = "dsfds33@3343";
+ 
 });
